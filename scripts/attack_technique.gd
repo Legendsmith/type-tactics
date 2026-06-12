c4bd73f8b91463de6eb1d75493fd9705191d8eed
Legendsmith <legendsmith.au@gmail.com>
2026-06-12 12:12:02 +1000

    Update attack_technique.gd
    Fix malformed reference.

--- scripts/attack_technique.gd
+++ scripts/attack_technique.gd
@@ -1,8 +1,8 @@
 class_name AttackTechnique
 extends BattleTechnique
 @export var power:int = 100
-@export var base:Units.Attribute = Units.Attribute.ATTACK
-@export var target:Units.Attribute = Units.Attribute.DEFENSE
+@export var base:Unit.Attribute = Unit.Attribute.ATTACK
+@export var target:Unit.Attribute = Unit.Attribute.DEFENSE
 
 func execute() -> bool:
 	return true
