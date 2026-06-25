71c1a4f5c6433d35d62a48d45d9eb7f7ab540c4c
Legendsmith <legendsmith.au@gmail.com>
2026-06-25 21:01:38 +1000

    Begin Gridmap implementation

--- world/battlefield_3d.gd
+++ world/battlefield_3d.gd
@@ -1,1 +1,13 @@
 extends Node3D
+@export var width:int = 3
+@export var grid:Dictionary[Vector2i,Node3D]
+
+
+func _ready() -> void:
+	var children:Array[Node] = get_children()
+	var last:int = 0
+	for child:Node3D in children:
+		var key:Vector2i = Vector2i
+		grid
+
+
