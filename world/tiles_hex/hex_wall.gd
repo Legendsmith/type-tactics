7bb267ea8d76f28b7fabd9b16c8145f6a30b518d
Legendsmith <legendsmith.au@gmail.com>
2026-06-30 03:31:54 +1000

    worldmap builder addon

--- world/tiles_hex/hex_wall.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends "res://world/tiles_hex/hex.gd"
-
-func update_color():
-	var parent: TileMapLayer = get_parent()
-	var new_color = parent.tile_set.get_terrain_color(terrain_set, terrain).lightened(0.1)
-	color = new_color
-	$WallTop.color = new_color
