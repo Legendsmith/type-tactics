090a72c737dfbd400182cf9d4057d810c5360de4
Pheubel <luca.guagliardo@live.nl>
2026-07-24 01:38:36 +0200

    add audio bus

--- globals/constants.gd
+++ globals/constants.gd
@@ -1,3 +1,17 @@
 class_name Constants
 const PLAYER_GROUP := &"player"
 const ENEMY_GROUP := &"opponent"
+
+#region Adio Bus Information
+
+const MASTER_BUS: StringName = &"Master"
+const MUSIC_BUS: StringName = &"Music"
+const SFX_BUS: StringName = &"Sfx"
+const CHARACTER_VOICING: StringName = &"Character Voicing"
+
+const MASTER_BUS_INDEX: int = 0
+const MUSIC_BUS_INDEX: int = 1
+const SFX_BUS_INDEX: int = 2
+const CHARACTER_VOICING_INDEX: int = 3
+
+#endregion
