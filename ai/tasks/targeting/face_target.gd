aad6270a1b07c863c26f93478425047f6d3283b5
Legendsmith <legendsmith.au@gmail.com>
2026-07-27 06:40:45 +1000

    Targeting BT Tasks

---
+++ ai/tasks/targeting/face_target.gd
@@ -0,0 +1,8 @@
+extends BTAction
+@export var target_var:StringName = &"target"
+#export_range(0,180,0.01,"radians_as_degrees") var tolerance:float = PI*.012
+
+func _tick(_delta: float) -> Status:
+	var target_point:Vector2 = blackboard.get_var(target_var).global_position
+	agent.animation_player.play("idle_"+str(Constants.get_direction_index(agent.global_positon.direction_to(target_point))))
+	return SUCCESS
