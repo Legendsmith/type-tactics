2103f94a0803834d6fb25e4c02260c1783efcf02
Legendsmith <legendsmith.au@gmail.com>
2026-08-04 14:48:50 +1000

    Create nav link script file

--- globals/spatial_map.gd
+++ globals/spatial_map.gd
@@ -4,4 +4,19 @@ signal request_hashmap_near(list: Array, coordinates: Vector2i)
 signal request_hashmap_near_filter(list: Array, coordinates: Vector2i, method: StringName, method_value: Variant)
 signal activate_grid(coordinates:Vector2i)
 
+var astar:AStar2D = AStar2D.new()
+var last_astar_idx:int = 0
 #var map:Dictionary[Vector2i,Node2D]
+
+func _ready() -> void:
+	pass
+
+func reset_map():
+	astar = AStar2D.new()
+
+func on_activate_grid(coordinates:Vector2i):
+	astar.add_point(last_astar_idx,coordinates,1)
+	last_astar_idx +=1
+
+func link_grid(from:Vector2,to:Vector2,bidirection:bool=true):
+	pass
