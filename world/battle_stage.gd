773394d40a206c42c21fc63ccfa37cf85992db45
Legendsmith <legendsmith.au@gmail.com>
2026-08-18 04:40:11 +1000

    Update battle_stage.gd
    Remove in heritance from MainScene2D
    Battle stage doesn't load interface to GameManager

--- world/battle_stage.gd
+++ world/battle_stage.gd
@@ -1,10 +1,11 @@
-extends MainScene2D
+extends Node2D
 
 signal finalize_turn
 signal new_turn
 signal unit_added(unit:Unit)
 signal battle_over(winner:StringName)
 
+@export var background_music:AudioStream
 static var battle_unit:PackedScene = load("uid://bm03ut2gnfrq8")
 
 var turn_ready:bool = false
@@ -15,7 +16,8 @@ var units_enemy:Dictionary[Unit,CombatMechanics.UnitStatus]
 
 
 func _ready():
-	super()
+	if background_music:
+		GameManager.play_music(background_music)
 	get_tree().call_group(Unit.UNIT_GROUP,&"battle_setup")
 
 
