8dba383eeee308f798b0d99163f6aeee3fb19b0e
Legendsmith <legendsmith.au@gmail.com>
2026-08-30 20:08:52 +1000

    Generated UnitDefs for overworld sprites.

--- scripts/generate_unit_defs.gd
+++ scripts/generate_unit_defs.gd
@@ -1,17 +1,37 @@
 @tool
 extends Node
-@export var save:bool = false
+signal request_generate
+signal request_save
+@export var generate:bool = false:
+	set(new):
+		generate = new
+		if generate:
+			request_generate.emit()
+@export var save:bool = false:
+	set(new):
+		save = new
+		if save:
+			request_save.emit()
+
 const SAVE_DIRECTORY = "res://data"
 @export var name_list:Dictionary[String,Texture2D]
 @export var generated_resources:Array[UnitDef]
 
-func _ready() -> void:
-	generate_names_from_overworld_sprites()
-	generate_unit_defs_from_names()
-	save_generated_resources()
 
-func generate_names_from_overworld_sprites():
+func _ready() -> void:
+	name = "UnitDefGenerator"
+	request_generate.connect(_on_request_generate)
+	request_save.connect(save_generated_resources)
+
+func _on_request_generate():
+	if generate:
+		generate_names_from_overworld_sprites()
+		generate_unit_defs_from_names()
+		generate = false
+
+func generate_names_from_overworld_sprites() -> void:
 	var dir = DirAccess.open("res://texture")
+	var skipped_names:Array[String]
 	if dir:
 		var files:PackedStringArray = dir.get_files()
 		for file_name:String in files:
@@ -19,7 +39,14 @@ func generate_names_from_overworld_sprites():
 				var split:PackedStringArray = file_name.split("_")
 				var unit_name:String = split[0]
 				var texture:Texture2D = load("res://texture".path_join(file_name))
-				name_list[unit_name] = texture
+				var resource_name:String = "unitdef_%s.tres" % unit_name
+				var file_path:String = SAVE_DIRECTORY.path_join(resource_name)
+				if FileAccess.file_exists(file_path):
+					skipped_names.append(unit_name)
+				else:
+					name_list[unit_name] = texture
+	if skipped_names.size():
+		print("Skipped existing definitions:" + ", ".join(skipped_names))
 
 func generate_unit_defs_from_names():
 	for unit_name:String in name_list.keys():
@@ -36,15 +63,19 @@ func generate_unit_defs_from_names():
 
 func save_generated_resources():
 	print_debug("Saving Generated resources")
+	var skipped_names:Array[String]
 	if save:
 		for resource:UnitDef in generated_resources:
-			var filename:String = "unitdef_%s.tres" % resource.twitch_name
+			var filename:String = "unitdef_%s.tres" % resource.unit_name
 			var file_path:String = SAVE_DIRECTORY.path_join(filename)
 			if FileAccess.file_exists(file_path):
+				skipped_names.append(filename)
 				continue
 			var err = ResourceSaver.save(resource, file_path)
 			if err == OK:
 				print("Saved: ", file_path)
 		save = false
+	if skipped_names.size():
+		print("Skipped existing Definitions:" + ", ".join(skipped_names))
 
 
